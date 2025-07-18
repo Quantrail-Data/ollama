460d671b9d65615d6352c2c76334cec685e0c87b
Janakiraman <jkr@quantrail-data.com>
2025-07-18 10:27:46 +0530

    add usage

--- envconfig/config.go
+++ envconfig/config.go
@@ -186,6 +186,10 @@ var (
 	ContextLength = Uint("OLLAMA_CONTEXT_LENGTH", 4096)
 	// Auth enables authentication between the Ollama client and server
 	UseAuth = Bool("OLLAMA_AUTH")
+	// Username returns the username for authentication
+	Username = String("OLLAMA_USERNAME")
+	// Password returns the password for authentication
+	Password = String("OLLAMA_PASSWORD")
 )
 
 func String(s string) func() string {
@@ -271,6 +275,8 @@ func AsMap() map[string]EnvVar {
 		"OLLAMA_MULTIUSER_CACHE":   {"OLLAMA_MULTIUSER_CACHE", MultiUserCache(), "Optimize prompt caching for multi-user scenarios"},
 		"OLLAMA_CONTEXT_LENGTH":    {"OLLAMA_CONTEXT_LENGTH", ContextLength(), "Context length to use unless otherwise specified (default: 4096)"},
 		"OLLAMA_NEW_ENGINE":        {"OLLAMA_NEW_ENGINE", NewEngine(), "Enable the new Ollama engine"},
+		"OLLAMA_USERNAME":          {"OLLAMA_USERNAME", Username(), "Username for authentication"},
+		"OLLAMA_PASSWORD":          {"OLLAMA_PASSWORD", Password(), "Password for authentication"},
 
 		// Informational
 		"HTTP_PROXY":  {"HTTP_PROXY", String("HTTP_PROXY")(), "HTTP proxy"},
